2b50bfe3340196f55ecfa8b8bc3fe8807f6cf930
josephfxw <josephxwf@gmail.com>
2017-06-28 10:59:46 -0400

    add project

---
+++ app/src/main/java/com/josephxwf/streamingmusic/Constants.java
@@ -0,0 +1,19 @@
+package com.josephxwf.streamingmusic;
+
+/**
+ * Created by grant on 08/02/2016.
+ */
+public class Constants {
+    public interface ACTION {
+        public static String MAIN_ACTION = "com.josephxwf.streamingmusic.action.main";
+        public static String PREV_ACTION = "com.josephxwf.streamingmusic.action.prev";
+        public static String PLAY_ACTION = "com.josephxwf.streamingmusic.action.play";
+        public static String NEXT_ACTION = "com.josephxwf.streamingmusic.action.next";
+        public static String STARTFOREGROUND_ACTION = "com.josephxwf.streamingmusic.action.startforeground";
+        public static String STOPFOREGROUND_ACTION = "com.josephxwf.streamingmusic.action.stopforeground";
+    }
+
+    public interface NOTIFICATION_ID {
+        public static int FOREGROUND_SERVICE = 101;
+    }
+}
---
+++ app/src/main/java/com/josephxwf/streamingmusic/Song.java
@@ -0,0 +1,52 @@
+package com.josephxwf.streamingmusic;
+
+/**
+ * Created by grant on 12/02/2016.
+ */
+public class Song {
+
+    int Id;
+    String title;
+    int numplays;
+    int numlikes;
+    String artist;
+
+    public Song (String Id, String title, String numplays, String numlikes, String aritst) {
+        try {
+            this.Id = Integer.parseInt(Id);
+        } catch (Exception e) {
+            this.Id = 0;
+        }
+        this.title = title;
+        try {
+            this.numplays = Integer.parseInt(numplays);
+        } catch (Exception e) {
+            this.numplays = 0;
+        }
+        try {
+            this.numlikes = Integer.parseInt(numlikes);
+        } catch (Exception e) {
+            this.numlikes = 0;
+        }
+        this.artist = aritst;
+    }
+
+    public int getId () {
+        return Id;
+    }
+
+    public String getTitle () {
+        return title;
+    }
+
+    public int getNumplays(){
+        return numplays;
+    }
+
+    public int getNumlikes () {
+        return numlikes;
+    }
+    public String getArtist () {
+        return artist;
+    }
+}
